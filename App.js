aa009d59ace7d2761bed1e5599b5bc8abfaecee9
Roberto Bernardes <roberto.bernardes@inema.ba.gov.br>
2018-05-28 12:58:02 -0300

    Iniciando projeto

---
+++ App.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,  
+  FlatList
+} from 'react-native';
+
+import Post from './components/Post';
+
+
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' +
+    'Cmd+D or shake for dev menu',
+  android: 'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+type Props = {};
+export default class App extends Component<Props> {
+
+  constructor() {
+    super();
+    this.state = {
+      fotos: []
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://instalura-api.herokuapp.com/api/public/fotos/rafael')
+        .then(resposta => resposta.json())
+        .then(json => this.setState({fotos: json}));
+  }
+
+  render() {
+    return (
+      <FlatList style={style.container}
+        keyExtractor={item => String(item.id)}
+        data={this.state.fotos}
+        renderItem={({item}) =>
+          <Post foto={item} />
+        }
+      />
+    );
+  }
+}
+
+const style = StyleSheet.create({
+  container: { 
+    marginTop: 20 
+  },
+});
