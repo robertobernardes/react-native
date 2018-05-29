0d7a0d5bc15aff9efe03c8d253f713d68cb37256
Roberto Bernardes <roberto.bernardes@inema.ba.gov.br>
2018-05-29 13:48:59 -0300

    Continuação do curso

--- components/Post.js
+++ components/Post.js
@@ -1,22 +1,77 @@
 import React, { Component } from 'react';
-import { View, Text, Image, StyleSheet, Dimensions } from 'react-native';
+import { View, Text, Image, StyleSheet, Dimensions, TouchableOpacity } from 'react-native';
 
 const width = Dimensions.get('screen').width;
 
 export default class Post extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            foto: {...this.props.foto, likers: [{}]}
+        }
+    }
+
+    like() {
+        const fotoAtualizada = {
+            ...this.state.foto,
+            likeada: !this.state.foto.likeada
+        };
+        this.setState({foto: fotoAtualizada});
+    }
+
+    carregaIcone(likeada) {
+        return likeada ? require('../assets/img/s2-checked.png') : require('../assets/img/s2.png');
+    }
+
+    exibeLikes(likers) {
+        if(likers.length <= 0)
+            return;
+
+        return (
+            <Text style={style.likes}> 
+                {likers.length} {likers.length > 1 ? 'curtidas': 'curtida'}
+            </Text>
+        );
+    }
+
+    exibeLegenda(foto) {
+        if(foto.comentario == '')
+            return;
+            
+        return (
+            <View style={style.comentario}>
+                <Text style={style.tituloComentario}>{foto.loginUsuario}</Text>
+                <Text>{foto.comentario}</Text>
+            </View>
+        );
+    }
+
     render() {
+        const { foto } = this.state;
         return (
             <View>
                 <View style={style.cabecalho}>
-                <Image source={{uri: this.props.foto.urlPerfil}}
+                <Image source={{uri: foto.urlPerfil}}
                     style={style.fotoDePerfil}
                 />
-                <Text>{this.props.foto.loginUsuario}</Text>
+                <Text>{foto.loginUsuario}</Text>
                 </View>            
-                <Image source={{uri: this.props.foto.urlFoto}}
-                style={style.foto}
-                />
+                <Image source={{uri: foto.urlFoto}}
+                    style={style.foto}/>
+
+                <View style={style.rodape}>
+                    <TouchableOpacity 
+                        onPress={this.like.bind(this)}>                    
+                        <Image source={this.carregaIcone(foto.likeada)} 
+                            style={style.botaoDeLike} />                        
+                    </TouchableOpacity>
+
+                    { this.exibeLikes(foto.likers) }
+                    
+                    { this.exibeLegenda(foto) }
+                    
+                 </View>
             </View>
         );
     }
@@ -38,4 +93,22 @@ const style = StyleSheet.create({
       width: width, 
       height: width
     },
+    botaoDeLike: {
+        marginBottom: 10,
+        height: 40,
+        width: 40
+    },
+    rodape: {
+        margin: 10
+    },
+    likes: {
+        fontWeight: 'bold'
+    },
+    comentario: {
+        flexDirection: 'row'
+    },
+    tituloComentario: {
+        fontWeight: 'bold',
+        marginRight: 5
+    }
   });
